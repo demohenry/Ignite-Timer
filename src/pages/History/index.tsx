82e1ca62214e30c8ed3b431af90a6f77c7e00165
Democrito <demohenry6@gmail.com>
2024-03-26 10:39:24 -0300

    Elabora funcionamento de styled component Status para dinamica de alteração de cor

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -21,7 +21,7 @@ export function History() {
              <td>20 minutos</td>
              <td>Há cerca de 4 minutos</td>
              <td>
-               <Status></Status>
+               <Status statusColor='red'>Interrompido</Status>
              </td>
            </tr>
            <tr>
@@ -29,7 +29,7 @@ export function History() {
              <td>20 minutos</td>
              <td>Há cerca de 4 minutos</td>
              <td>
-                <Status></Status>
+                <Status statusColor='yellow'>Em andamento</Status>
              </td>
            </tr>
            <tr>
@@ -37,7 +37,7 @@ export function History() {
              <td>20 minutos</td>
              <td>Há cerca de 4 minutos</td>
              <td>
-              <Status></Status>
+              <Status statusColor='yellow'>Em andamento</Status>
              </td>
            </tr>
            <tr>
@@ -45,7 +45,7 @@ export function History() {
              <td>20 minutos</td>
              <td>Há cerca de 4 minutos</td>
              <td>
-                <Status></Status>
+                <Status statusColor='green'>Concluído</Status>
              </td>
            </tr>
          </tbody>
