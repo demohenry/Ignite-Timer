3c441d28afce97611ec26826430fd7131dd1cbe8
Democrito <demohenry6@gmail.com>
2024-03-26 09:55:27 -0300

    Estruturação inicial e estilização de tabela de pagina de Historico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,59 @@
+import { HistoryContainer, HistoryList, Status } from "./styles"
 
 
 export function History() {
   return (
-    <>
-    <h1>History</h1>
-    </>
+    <HistoryContainer>
+     <h1>Meu Histórico</h1>
+     <HistoryList>
+       <table>
+         <thead>
+           <tr>
+             <th>Tarefa</th>
+             <th>Duração</th>
+             <th>Início</th>
+             <th>Status</th>
+           </tr>
+         </thead>
+         <tbody>
+           <tr>
+             <td>Fazer componente</td>
+             <td>20 minutos</td>
+             <td>Há cerca de 4 minutos</td>
+             <td>
+               <Status></Status>
+             </td>
+           </tr>
+           <tr>
+             <td>Fazer componente</td>
+             <td>20 minutos</td>
+             <td>Há cerca de 4 minutos</td>
+             <td>
+                <Status></Status>
+             </td>
+           </tr>
+           <tr>
+             <td>Fazer componente</td>
+             <td>20 minutos</td>
+             <td>Há cerca de 4 minutos</td>
+             <td>
+              <Status></Status>
+             </td>
+           </tr>
+           <tr>
+             <td>Fazer componente</td>
+             <td>20 minutos</td>
+             <td>Há cerca de 4 minutos</td>
+             <td>
+                <Status></Status>
+             </td>
+           </tr>
+         </tbody>
+         <tbody>
+
+         </tbody>
+       </table>
+     </HistoryList>
+    </HistoryContainer>
   )
 }
